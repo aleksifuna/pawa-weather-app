196bfcfeacc6bea87c863b0842e09b5faabc62e7
alex <aleksifuna@gmail.com>
2025-04-23 11:08:45 +0300

    fixed the search form to dynamically fetch data from the backend

--- frontend/app/SearchBar.tsx
+++ frontend/app/SearchBar.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+'use client';
 
-export const SearchBar = () => {
+import React, { FormEvent, useState } from "react";
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+export const SearchBar: React.FC<SearchBarProps> = ({onSearch}) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (searchQuery.trim()) {
+      onSearch(searchQuery)
+    }
+  };
   return (
     <div className="flex justify-between">
-      <form className="flex w-full max-w-3xl gap-x-2">
+      <form onSubmit={handleSubmit} className="flex w-full max-w-3xl gap-x-2">
         <input
           type="text"
           placeholder="Search City.."
+          value={searchQuery ?? ""}
+          onChange={(e)=>setSearchQuery(e.target.value)}
           className="basis-[80%] px-4 py-2 bg-gray-800 text-white focus:outline-none"
         />
         <button
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import CurrentWeather from "./CurrentWeather";
 import { DateLocation } from "./DateLocation";
 import { Forecast } from "./Forecast";
@@ -5,28 +7,54 @@ import { SearchBar } from "./SearchBar";
 import { WindHumidity } from "./WindHumidity";
 
 export default function Home() {
-  const data = {
-    location: "Nairobi",
-    temperature: 16.64,
-    icon: "04n",
-    description: "overcast clouds",
-    wind_status: 2.27,
-    humidity: 93,
-    dt: 1745354572,
-  };
+const [weatherData, setWeatherData] = useState({
+  location: "Nairobi",
+  temperature: 16.64,
+  icon: "04n",
+  description: "overcast clouds",
+  wind_status: 2.27,
+  humidity: 93,
+  dt: 1745354572,
+});
+
+const fetchWeatherData = async (location: String) => {
+  
+  try {
+    const response = await fetch(
+      `http://127.0.0.1:8000/api/weather/${location}`
+    );
+    
+    if (!response.ok) {
+      throw new Error("Could not fetch weather data");
+    }
+    
+    const data = await response.json();
+    setWeatherData({
+      location: data.location,
+      temperature: data.temperature,
+      icon: data.icon,
+      description: data.description,
+      wind_status: data.wind_status,
+      humidity: data.humidity,
+      dt: data.dt,
+    });
+  } catch (err) {
+    throw new Error("Could not fetch weather data");
+  }
+};
 
   return (
     <div className="grid grid-cols-4 gap-x-1 max-w-[screen] h-screen bg-gray-950 text-white p-0.5">
       <div className="col-span-1 flex flex-col justify-evenly bg-gray-900">
         <div>
-          <CurrentWeather data={data} />
+          <CurrentWeather data={weatherData} />
         </div>
         <div>
-          <DateLocation data={data} />
+          <DateLocation data={weatherData} />
         </div>
       </div>
       <div className="col-span-3 flex flex-col justify-evenly bg-gray-900 p-2">
-        <SearchBar />
+        <SearchBar onSearch={fetchWeatherData}/>
         <Forecast />
         <WindHumidity />
       </div>
